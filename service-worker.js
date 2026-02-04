f5782d4fa006041047021ecb407226dcd21b84e5
Yehonatan Ravoach <168662146+YehonatanRavoach@users.noreply.github.com>
2026-02-04 22:47:01 +0200

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'thailand-trip-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
